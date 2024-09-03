091288232bf74cadcc47d0f15d3c7bcd87998af5
JonathanEduardo <jhonquistiano@gmail.com>
2024-09-03 15:57:57 -0600

    Martes

---
+++ IntroduccionAEFcore/Controllers/ActoresController.cs
@@ -0,0 +1,38 @@
+﻿using IntroduccionAEFcore.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroduccionAEFcore.Controllers
+{
+    [ApiController]
+    [Route("api/actores")]
+    public class ActoresController: ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ActoresController(ApplicationDbContext context)
+        {
+
+            _context = context;
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Actor actor)
+        {
+            //segun llega todo el grafo  para meter info hasta de pelicula y para eso se crea DTOs
+            _context.Add(actor);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPost("varios")]
+        public async Task<ActionResult> Post(Actor[] actores)
+        {
+            //segun llega todo el grafo  para meter info hasta de pelicula y para eso se crea DTOs
+            _context.AddRange(actores);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
---
+++ IntroduccionAEFcore/Controllers/GenerosController.cs
@@ -0,0 +1,36 @@
+﻿using IntroduccionAEFcore.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroduccionAEFcore.Controllers
+{
+    [ApiController]
+    [Route("api/generos")]
+    public class GenerosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public GenerosController(ApplicationDbContext context)
+        {
+
+            _context = context;
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Genero genero)
+        {
+            //segun llega todo el grafo  para meter info hasta de pelicula y para eso se crea DTOs
+            _context.Add(genero);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPost("varios")]
+        public async Task<ActionResult> Post(Genero[] generos)
+        {
+            //segun llega todo el grafo  para meter info hasta de pelicula y para eso se crea DTOs
+            _context.AddRange(generos);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
